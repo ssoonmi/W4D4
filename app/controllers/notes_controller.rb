afad3550b8b6ddf37774bf077673246cd7d8e349
App Academy Student <diego.rodriguez923@gmail.com>
2018-10-18 18:08:20 -0400

    Finish W4D4 project music app

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,34 @@
+class NotesController < ApplicationController
+  before_action :ensure_logged_in
+
+  def create
+    note = Note.new(note_params)
+    note.user_id = current_user.id
+    if note.save
+
+    else
+      flash[:errors] = note.errors.full_messages
+    end
+    redirect_to track_url(note.track_id)
+  end
+
+  def destroy
+    note = Note.find(params[:id])
+    if note && current_user.id == note.user_id
+      track_id = note.track_id
+      note.destroy
+      redirect_to track_url(track_id)
+    elsif note
+      track_id = note.track_id
+      redirect_to track_url(track_id), status: 403
+    else
+      redirect_to bands_url
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:track_id, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create, :show]
+
+  resource :session, only: [:new, :create, :destroy]
+
+  resources :bands, only: [:index, :create, :new, :edit, :show, :update, :destroy] do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, only: [:create, :edit, :index, :show, :update, :destroy] do
+    resources :tracks, only: [:new]
+  end
+
+  resources :tracks, only: [:create, :edit, :index, :show, :update, :destroy] 
+
+  resources :notes, only: [:create, :destroy]
+
+end
